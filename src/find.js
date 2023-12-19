d690fea2d004465495fdf904f4c784d3c2c0d271
Tihons <tihonshramenko@gmail.com>
2023-12-19 16:41:59 +0200

    save

--- src/find.js
+++ src/find.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api';
 const apiKey = '41103551-afb5440a0c91585482e280fcd';
 
+
 export async function search(q, page, perPage) {
   const params = new URLSearchParams({
     key: apiKey,
